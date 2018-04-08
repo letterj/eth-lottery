3511a1916ab8064c94aad165388f991804e787ab
Jay Payne <letterj@gmail.com>
2018-04-08 09:49:41 -0500

    Initial commit

---
+++ deploy.js
@@ -0,0 +1,25 @@
+// WIP [Work in Progress] 
+
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3')
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    <INSERT BIP39 passcode string>,  
+    <INSERT Rinkeby address for infra>
+);
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to ', result.options.address);
+};
+deploy();
+// 0x270DCCA072fFBb1c953a62f51C7451C02313D06c
